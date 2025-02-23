467a69a0e842697c6be11bed6d7b390cbd410a4c
Justin Bueno <justinbueno63@gmail.com>
2025-02-23 05:03:46 -0800

    Configure React app for deployment to GitHub Pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -38,6 +38,7 @@ class App extends Component {
         }
         return (    
             <div className="tc">
+                <h1>EXAMPLE EXAMPLE</h1>
                 <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange = {this.onSearchChange}/>
                 <Scroll>
